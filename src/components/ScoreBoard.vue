4c050f4353736ee15060050eb0635a30a86d73dc
Dino Ribeiro <DRibeiro1124@gmail.com>
2018-09-05 09:53:40 -0400

    added away team and functions.  Buttons seems to be working

--- src/components/ScoreBoard.vue
+++ src/components/ScoreBoard.vue
@@ -4,7 +4,7 @@
             <h1>NBA Score Board</h1>
         </header>
         <section class="main-box">
-            <div class="home-team"></div>
+            <div class="home-team">
             <p>Home Team</p>
             <h3>{{team1DisplayName}}</h3>
             <p class="score">{{team1_score}}</p>
@@ -19,6 +19,23 @@
                 <button v-on:click="homeTeamAdd3">Add 3</button>  
                 <button v-on:click="homeTeamSubtract1">Subtract 1</button>  
             </div>
+            </div>
+            <div class="away-team">
+            <p>Away Team</p>
+            <h3>{{team2DisplayName}}</h3>
+            <p class="score">{{team2_score}}</p>
+            <div>
+                <input v-model="team2Name" type="text" placeholder="Update Away Team Name">
+                <button v-on:click="t2_update_name">Update</button>
+            </div>
+            <div>
+                <p>Update Away Team Score</p>
+                <button v-on:click="awayTeamAdd1">Add 1</button>  
+                <button v-on:click="awayTeamAdd2">Add 2</button>  
+                <button v-on:click="awayTeamAdd3">Add 3</button>  
+                <button v-on:click="awayTeamSubtract1">Subtract 1</button>  
+            </div>
+            </div>
         </section>
     </div>
 </template>
@@ -29,9 +46,13 @@ export default {
     data: function () {
         return {
             team1Name: "",
+            team2Name: "",
             team1DisplayName: "",
+            team2DisplayName: "",
             t1_counter: 0,
+            t2_counter: 0,
             team1_score: 0,
+            team2_score: 0,
 
         }
     },
@@ -59,6 +80,31 @@ export default {
         homeTeamSubtract1: function () {
             console.log("take one away")
             this.team1_score --;
+        },
+
+        t2_update_name: function () {
+            console.log("updated")
+            this.team2DisplayName = this.team2Name
+        },
+
+        awayTeamAdd1: function () {
+            console.log("add one is being clicked")
+            this.team2_score++;
+        },
+
+        awayTeamAdd2: function () {
+            console.log("add two")
+            this.team2_score += 2;
+        },
+
+        awayTeamAdd3: function () {
+            console.log("add 3")
+            this.team2_score += 3;
+        },
+
+        awayTeamSubtract1: function () {
+            console.log("take one away")
+            this.team2_score --;
         }
         
     }
